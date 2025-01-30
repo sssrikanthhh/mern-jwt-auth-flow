f30d69dc12eccfca32c5a049d07e8a55a715aea1
srikanth <srikanth.thedev@gmail.com>
2025-01-30 14:39:54 +0000

    Add register route

---
+++ api/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import { hashValue } from '../utils/bcrypt-utils';
+
+export interface UserDocument extends mongoose.Document {
+  email: string;
+  password: string;
+  verified: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword: (val: string) => Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    verified: { type: Boolean, default: false, required: true }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = await hashValue(this.password);
+});
