0974eb84f6a0243cb2e43c6814baf636d0a2ed73
srikanth <srikanth.thedev@gmail.com>
2025-01-31 16:12:27 +0000

    Update errorHandler to handle AppErrors

--- api/src/middleware/errorHandler.ts
+++ api/src/middleware/errorHandler.ts
@@ -1,6 +1,7 @@
 import { ErrorRequestHandler, Response } from 'express';
 import z from 'zod';
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from '../constants/httpCodes';
+import { AppError } from '../utils/AppError';
 
 const handleZodError = (res: Response, err: z.ZodError) => {
   const errors = err.issues.map(issue => ({
@@ -13,10 +14,21 @@ const handleZodError = (res: Response, err: z.ZodError) => {
   });
 };
 
+const handleAppError = (res: Response, err: AppError) => {
+  return res.status(err.statusCode).json({
+    message: err.message,
+    errorCode: err.errorCode
+  });
+};
+
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log(`Path: ${req.path}, `, err);
   if (err instanceof z.ZodError) {
     handleZodError(res, err);
+    return;
+  }
+  if (err instanceof AppError) {
+    handleAppError(res, err);
     return;
   }
   res.status(INTERNAL_SERVER_ERROR).json({ error: 'Internal server error' });
