65fe8d9b3bade17a20b42ebc4b1d635a701e3b2b
srikanth <srikanth.thedev@gmail.com>
2025-01-31 11:45:31 +0000

    update env constants

--- api/src/constants/env.ts
+++ api/src/constants/env.ts
@@ -9,3 +9,6 @@ export const getEnv = (key: string, defaultValue?: string): string => {
 export const MONGO_URI = getEnv('MONGO_URI');
 export const PORT = getEnv('PORT', '8008');
 export const APP_ORIGIN = getEnv('APP_ORIGIN');
+export const JWT_SECRET = getEnv('JWT_SECRET');
+export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
+export const NODE_ENV = getEnv('NODE_ENV');
