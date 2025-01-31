4054812117170a253e8892d8db007297740ea549
srikanth <srikanth.thedev@gmail.com>
2025-01-31 11:45:09 +0000

    Add date utilities

---
+++ api/src/utils/date.ts
@@ -0,0 +1,10 @@
+export function oneYearFromNow() {
+  return new Date(Date.now() + 365 * 24 * 60 * 60 * 1000);
+}
+
+export function thirtyDaysFromNow() {
+  return new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+}
+export function fifteenMinutesFromNow() {
+  return new Date(Date.now() + 15 * 60 * 1000);
+}
