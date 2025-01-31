c12ce049602ca7cf140c3ce436dbbcd942164304
srikanth <srikanth.thedev@gmail.com>
2025-01-31 07:26:43 +0000

    Update bcrypt-utils name

--- api/src/utils/bcrypt.ts
+++ api/src/utils/bcrypt.ts
@@ -2,3 +2,6 @@ import bcrypt from 'bcrypt';
 
 export const hashValue = async (value: string, saltRound?: number) =>
   await bcrypt.hash(value, saltRound || 10);
+
+export const compareValue = async (value: string, hash: string) =>
+  await bcrypt.compare(value, hash).catch(() => false);
