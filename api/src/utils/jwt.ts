d649236a3b84d7ae8ade8e6c061eaaff814bad27
srikanth <srikanth.thedev@gmail.com>
2025-02-01 03:49:26 +0000

    Add signToken utility method

---
+++ api/src/utils/jwt.ts
@@ -0,0 +1,42 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import { SessionDocument } from '../models/session.model';
+import { UserDocument } from '../models/user.model';
+import { JWT_REFRESH_SECRET, JWT_SECRET } from '../constants/env';
+
+export type AccessTokenPayload = {
+  sessionId: SessionDocument['_id'];
+  userId: UserDocument['_id'];
+};
+
+export type RefreshTokenPayload = {
+  sessionId: SessionDocument['_id'];
+};
+
+export type SignOptionsAndSecret = SignOptions & { secret: string };
+
+const defaultsSignOptions: SignOptions = {
+  audience: ['user']
+};
+
+export const accessTokenSignOptions: SignOptionsAndSecret = {
+  expiresIn: '15m',
+  secret: JWT_SECRET
+};
+export const refreshTokenSignOptions: SignOptionsAndSecret = {
+  expiresIn: '30d',
+  secret: JWT_REFRESH_SECRET
+};
+
+export const signToken = (
+  payload: AccessTokenPayload | RefreshTokenPayload,
+  options?: SignOptionsAndSecret
+) => {
+  //extract secret from options and use the rest as sign options
+  //if the options are not passed, use accessTokenSignOptions
+  const { secret, ...restOptions } = options || accessTokenSignOptions;
+  return jwt.sign(payload, secret, {
+    //if the options are passed, will override the defaults(audience)
+    ...defaultsSignOptions,
+    ...restOptions
+  });
+};
