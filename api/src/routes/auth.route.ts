fa68cc29bf2c217359fa75f33cdade8ed6092713
srikanth <srikanth.thedev@gmail.com>
2025-02-01 05:13:24 +0000

    Add logout route

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { loginHandler, registerHandler } from '../controllers/auth.controller';
+import {
+  loginHandler,
+  logoutHandler,
+  registerHandler
+} from '../controllers/auth.controller';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/register', registerHandler);
 authRoutes.post('/login', loginHandler);
+authRoutes.get('/logout', logoutHandler);
 
 export default authRoutes;
