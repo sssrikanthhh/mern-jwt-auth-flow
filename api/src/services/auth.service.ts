60d6865323c9ac8f8b32f762e85e0662e389a96d
srikanth <srikanth.thedev@gmail.com>
2025-02-01 03:49:54 +0000

    Add loginUser service

--- api/src/services/auth.service.ts
+++ api/src/services/auth.service.ts
@@ -1,4 +1,3 @@
-import jwt from 'jsonwebtoken';
 import sessionModel from '../models/session.model';
 import userModel from '../models/user.model';
 import verificationCodeModel from '../models/verificationCode.model';
@@ -6,7 +5,13 @@ import { oneYearFromNow } from '../utils/date';
 import { VerificationCodeType } from '../utils/verificationCodeType';
 import { JWT_REFRESH_SECRET, JWT_SECRET } from '../constants/env';
 import { appAssert } from '../utils/appAssert';
-import { CONFLICT } from '../constants/httpCodes';
+import { CONFLICT, UNAUTHORIZED } from '../constants/httpCodes';
+import { AppError } from '../utils/AppError';
+import {
+  accessTokenSignOptions,
+  refreshTokenSignOptions,
+  signToken
+} from '../utils/jwt';
 
 export type CreateAccountParams = {
   email: string;
@@ -15,45 +20,77 @@ export type CreateAccountParams = {
 };
 
 export const createAccount = async (data: CreateAccountParams) => {
+  //check for existing user
   const existingUser = await userModel.exists({ email: data.email });
+  // if the user already exists, throw an error
   appAssert(!existingUser, CONFLICT, 'This email is already taken');
 
+  //create a new user
   const user = await userModel.create({
     email: data.email,
     password: data.password
   });
 
+  //create a verification code
   const verificationCode = await verificationCodeModel.create({
     userId: user._id,
     type: VerificationCodeType.EmailVerification,
     expiresAt: oneYearFromNow()
   });
 
+  //create a user session
   const session = await sessionModel.create({
     userId: user._id,
     userAgent: data.userAgent
   });
 
-  const refreshToken = jwt.sign(
-    {
-      sessionId: session._id
-    },
-    JWT_REFRESH_SECRET,
-    {
-      expiresIn: '30d',
-      audience: ['user']
-    }
+  //create refresh and access tokens
+  const refreshToken = signToken(
+    { sessionId: session._id },
+    refreshTokenSignOptions
   );
-  const accessToken = jwt.sign(
-    {
-      sessionId: session._id,
-      userId: user._id
-    },
-    JWT_SECRET,
-    {
-      expiresIn: '15m',
-      audience: ['user']
-    }
+  const accessToken = signToken(
+    { sessionId: session._id, userId: user._id },
+    accessTokenSignOptions
+  );
+  return {
+    user: user.omitPassword(),
+    refreshToken,
+    accessToken
+  };
+};
+
+export type LoginUserParams = {
+  email: string;
+  password: string;
+  userAgent?: string;
+};
+
+export const loginUser = async (data: LoginUserParams) => {
+  //find the user with the email to check if the user exists or not
+  const user = await userModel.findOne({ email: data.email });
+  //throw an error if the error doesn't exist
+  appAssert(user, UNAUTHORIZED, 'Invalid email or password');
+
+  //validate the password
+  const isValid = await user.comparePassword(data.password);
+  //throw an error, if the password is invalid
+  appAssert(isValid, UNAUTHORIZED, 'Invalid email or password');
+
+  //create a user session
+  const session = await sessionModel.create({
+    userId: user._id,
+    userAgent: data.userAgent
+  });
+
+  //create refresh and access tokens
+  const refreshToken = signToken(
+    { sessionId: session._id },
+    refreshTokenSignOptions
+  );
+  const accessToken = signToken(
+    { sessionId: session._id, userId: user._id },
+    accessTokenSignOptions
   );
   return {
     user: user.omitPassword(),
