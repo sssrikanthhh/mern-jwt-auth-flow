5dddad208a140f5f6ce2c3c424cb61f8e0b141d1
srikanth <srikanth.thedev@gmail.com>
2025-02-01 03:49:05 +0000

    Add login route to login router

--- api/src/routes/auth.route.ts
+++ api/src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { registerHandler } from '../controllers/auth.controller';
+import { loginHandler, registerHandler } from '../controllers/auth.controller';
 
 const authRoutes = express.Router();
 
 authRoutes.post('/register', registerHandler);
+authRoutes.post('/login', loginHandler);
 
 export default authRoutes;
