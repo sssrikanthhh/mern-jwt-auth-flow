7daca21fcaf412878ac847167570a38cec11e8c1
srikanth <srikanth.thedev@gmail.com>
2025-02-02 06:35:20 +0000

    Add now and one day in ms constants

--- api/src/utils/date.ts
+++ api/src/utils/date.ts
@@ -8,3 +8,6 @@ export function thirtyDaysFromNow() {
 export function fifteenMinutesFromNow() {
   return new Date(Date.now() + 15 * 60 * 1000);
 }
+
+export const NOW = Date.now();
+export const ONE_DAY_MS = 24 * 60 * 60 * 1000;
