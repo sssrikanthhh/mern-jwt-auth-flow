2bc80e831ebbcf0dca7975fffe584c82e9ed4ac0
srikanth <srikanth.thedev@gmail.com>
2025-02-11 07:32:24 +0000

    Add schema to validate reset password

--- api/src/schemas/auth.ts
+++ api/src/schemas/auth.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const emailSchema = z.string().min(1).max(255);
 export const passwordSchema = z.string().min(8).max(255);
+export const verificationCodeSchema = z.string().min(1).max(24);
 
 export const loginSchema = z.object({
   email: emailSchema,
@@ -18,4 +19,7 @@ export const registerSchema = loginSchema
     path: ['confirmPassword']
   });
 
-export const verificationCodeSchema = z.string().min(1).max(24);
+export const resetPasswordSchema = z.object({
+  verificationCode: verificationCodeSchema,
+  password: passwordSchema
+});
