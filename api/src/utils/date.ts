72261ef87b1545b6e84237a8875017b4c727ec35
srikanth <srikanth.thedev@gmail.com>
2025-02-11 06:48:44 +0000

    Add more date utilities

--- api/src/utils/date.ts
+++ api/src/utils/date.ts
@@ -9,5 +9,10 @@ export function fifteenMinutesFromNow() {
   return new Date(Date.now() + 15 * 60 * 1000);
 }
 
+export function oneHourFromNow() {
+  return new Date(Date.now() + 60 * 60 * 1000);
+}
+
 export const NOW = Date.now();
 export const ONE_DAY_MS = 24 * 60 * 60 * 1000;
+export const fiveMinAgo = () => new Date(Date.now() - 5 * 60 * 1000);
