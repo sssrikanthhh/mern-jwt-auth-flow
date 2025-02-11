3a5a85863ba82a382cd69d85d2d0c814658f1e53
srikanth <srikanth.thedev@gmail.com>
2025-02-11 14:14:28 +0000

    Add a route to delete a session

--- api/src/routes/session.route.ts
+++ api/src/routes/session.route.ts
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getSessionsHandler } from '../controllers/session.controller';
+import {
+  deleteSessionHandler,
+  getSessionsHandler
+} from '../controllers/session.controller';
 
 const sessionRoutes = express.Router();
 
 //prefix: /api/sessions
 //get sessions of a user
 sessionRoutes.get('/', getSessionsHandler);
+//delete session
+sessionRoutes.delete('/:id', deleteSessionHandler);
 export default sessionRoutes;
