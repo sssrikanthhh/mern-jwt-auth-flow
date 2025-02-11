af0355e07951779a351fa8a983de3b5a99b44886
srikanth <srikanth.thedev@gmail.com>
2025-02-11 06:49:03 +0000

    Add exports to email and password schema

--- api/src/schemas/auth.ts
+++ api/src/schemas/auth.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
-const emailSchema = z.string().min(1).max(255);
-const passwordSchema = z.string().min(8).max(255);
+export const emailSchema = z.string().min(1).max(255);
+export const passwordSchema = z.string().min(8).max(255);
 
 export const loginSchema = z.object({
   email: emailSchema,
