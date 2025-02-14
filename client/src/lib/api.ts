4ccba4ca9239818e3e108da25df25d116982d620
srikanth <srikanth.thedev@gmail.com>
2025-02-14 16:19:22 +0000

    Add a signup method

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,10 @@
 import { API } from '@/config/apiClient';
-import { SigninType } from '@/types/auth';
+import { SigninType, SignupType } from '@/types/auth';
 
 export const signin = async (data: SigninType) =>
   //make a post request to /api/auth/login
   API.post('/auth/login', data);
+
+export const signup = async (data: SignupType) =>
+  //make a post request to /api/auth/register
+  API.post('/auth/register', data);
