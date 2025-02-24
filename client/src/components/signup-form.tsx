779305fba22ab31aa4a4759c424d058fede5759d
srikanth <srikanth.thedev@gmail.com>
2025-02-24 13:18:38 +0000

    Implement signup functionality

--- client/src/components/signup-form.tsx
+++ client/src/components/signup-form.tsx
@@ -1,28 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-
-type InputData = {
-  email: string;
-  password: string;
-  confirmPassword: string;
-};
+import { SignupType } from '@/types/auth';
+import { useMutation } from '@tanstack/react-query';
+import { signup } from '@/lib/api';
 
 export function SignupForm() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, errors }
-  } = useForm<InputData>();
+  } = useForm<Omit<SignupType, 'userAgent'>>();
+  const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<InputData> = data => {
-    console.log(data);
+  //signup mutation
+  const {
+    mutate: signupUser,
+    isError,
+    isPending
+  } = useMutation({
+    mutationFn: signup,
+    onSuccess: () => {
+      navigate('/', {
+        replace: true
+      });
+    }
+  });
+
+  const onSubmit: SubmitHandler<Omit<SignupType, 'userAgent'>> = data => {
+    signupUser({
+      ...data,
+      userAgent: navigator.userAgent
+    });
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      {isError && (
+        <p className='text-red-500 text-center mb-1'>
+          Something went wrong. Please try again.
+        </p>
+      )}
       <div className='flex flex-col gap-6'>
         <div className='grid gap-2'>
           <Label htmlFor='email'>Email</Label>
@@ -79,8 +99,12 @@ export function SignupForm() {
             <p className='text-red-500'>{errors.password.message}</p>
           )}
         </div>
-        <Button type='submit' className='w-full' disabled={isSubmitting}>
-          Signup
+        <Button
+          type='submit'
+          className='w-full'
+          disabled={isSubmitting || isPending}
+        >
+          {isPending ? 'Signing up...' : 'Signup'}
         </Button>
       </div>
       <div className='mt-4 text-center text-sm'>
