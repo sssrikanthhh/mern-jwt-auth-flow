fab00a2004d86f34bf95584e72a4683381c6b356
srikanth <srikanth.thedev@gmail.com>
2025-02-25 06:07:13 +0000

    Register the reset password route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Auth from '@/pages/auth';
 import VerifyEmail from '@/pages/verify-email';
 import ForgotPassword from './pages/forgot-password';
+import ResetPassword from './pages/reset-password';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route path='/signup' element={<Auth />} />
       <Route path='/email/verify/:code' element={<VerifyEmail />} />
       <Route path='/password/forgot' element={<ForgotPassword />} />
+      <Route path='/password/reset' element={<ResetPassword />} />
     </Routes>
   );
 }
