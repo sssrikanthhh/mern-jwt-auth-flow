7a1a89a7d968c7d1878a09592e3e66248654c9a1
srikanth <srikanth.thedev@gmail.com>
2025-02-25 05:21:44 +0000

    Register the forgot password request route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Auth from '@/pages/auth';
 import VerifyEmail from '@/pages/verify-email';
+import ForgotPassword from './pages/forgot-password';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path='/signin' element={<Auth />} />
       <Route path='/signup' element={<Auth />} />
       <Route path='/email/verify/:code' element={<VerifyEmail />} />
+      <Route path='/password/forgot' element={<ForgotPassword />} />
     </Routes>
   );
 }
