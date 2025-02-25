42302b3c6b99dae0ffa9b809d7ae7b2115e1b341
srikanth <srikanth.thedev@gmail.com>
2025-02-25 14:04:46 +0000

    Add and setup react query devtools

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { queryClient } from './config/query-client.ts';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from './components/theme-provider.tsx';
 import App from './App.tsx';
 import './index.css';
@@ -14,6 +15,11 @@ createRoot(document.getElementById('root')!).render(
         <BrowserRouter>
           <App />
         </BrowserRouter>
+        <ReactQueryDevtools
+          position='bottom'
+          buttonPosition='bottom-left'
+          initialIsOpen={false}
+        />
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>
