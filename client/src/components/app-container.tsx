ac4e1ce9f60c5bb40b6f32993a30ea3d8b1e9e22
srikanth <srikanth.thedev@gmail.com>
2025-02-25 14:08:04 +0000

    Add hoc app container for protected routes

---
+++ client/src/components/app-container.tsx
@@ -0,0 +1,33 @@
+import { useFetchUser } from '@/hooks/useFetchUser';
+import Spinner from './spinner';
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function AppContainer() {
+  const { user, isLoading } = useFetchUser();
+
+  if (isLoading) {
+    return (
+      <div className='container max-auto w-screen h-screen flex items-center justify-center'>
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (!user) {
+    return (
+      <Navigate
+        to='/signin'
+        replace
+        state={{
+          redirectUrl: window.location.pathname
+        }}
+      />
+    );
+  }
+
+  return (
+    <div className='container mx-auto w-screen h-screen'>
+      <Outlet />
+    </div>
+  );
+}
