cfc8f61fb01543d41abbb357be4dfc00dff6fe31
srikanth <srikanth.thedev@gmail.com>
2025-02-25 14:07:17 +0000

    Add a api method to fetch the user

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,5 +1,6 @@
 import { API } from '@/config/apiClient';
 import { SigninType, SignupType } from '@/types/auth';
+import { User } from '@/types/user';
 
 export const signin = async (data: SigninType) =>
   //make a post request to /api/auth/login
@@ -10,7 +11,7 @@ export const signup = async (data: SignupType) =>
   API.post('/auth/register', data);
 
 export const verifyEmail = async (code: string) =>
-  //make a post request to /api/auth/email/verify/:code
+  //make a get request to /api/auth/email/verify/:code
   API.get(`/auth/email/verify/${code}`);
 
 export const sendPasswordResetEmail = async (email: string) =>
@@ -26,3 +27,8 @@ export const resetPassword = async ({
 }) =>
   //make a post request to /api/auth/password/reset
   API.post('/auth/password/reset', { verificationCode, password });
+
+//user api
+export const fetchUser = async (): Promise<User> =>
+  //make a get request to /api/user
+  API.get('/user', { withCredentials: true });
