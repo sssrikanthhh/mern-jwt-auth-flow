b6b35451f25114f3d28a1b5ae4c93e8b1dddeb06
srikanth <srikanth.thedev@gmail.com>
2025-02-26 07:12:42 +0000

    Update the api client to refresh the access token

--- client/src/config/apiClient.ts
+++ client/src/config/apiClient.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { queryClient } from './query-client';
+import { navigate } from '@/lib/navigation';
 
 const options = {
   baseURL: import.meta.env.VITE_API_URL,
@@ -6,12 +8,34 @@ const options = {
 };
 
 export const API = axios.create(options);
+//create a client for refreshing the access token - to avoid infinite loop with the error interceptor
+const TokenRefreshClient = axios.create(options);
+TokenRefreshClient.interceptors.response.use(response => response.data);
 
 //interceptors
 API.interceptors.response.use(
   response => response.data,
-  error => {
-    const { status, data } = error.response;
+  async error => {
+    const { response, config } = error;
+    const { status, data } = response || {};
+
+    //refresh the access token if the status is 401
+    if (status === 401 && data?.errorCode === 'INVALID_ACCESS_TOKEN') {
+      try {
+        await TokenRefreshClient.get('/auth/refresh');
+
+        return TokenRefreshClient(config);
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        queryClient.clear();
+        navigate('/signin', {
+          state: {
+            redirectUrl: window.location.pathname
+          }
+        });
+      }
+    }
+
     return Promise.reject({ status, ...data });
   }
 );
